e4a737b9f5fe04b34bf680e2c485318f6153dc11
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-07-26 19:08:02 -0300

    feat: add repositories

---
+++ src/MoviesApi.Domain/Interfaces/Services/MovieService/IMovieService.cs
@@ -0,0 +1,15 @@
+﻿using MoviesApi.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MoviesApi.Domain.Interfaces.Services.MovieService
+{
+    public interface IMovieService
+    {
+        Task<IEnumerable<Movie>> GetAllAsync();
+        Task<Movie> GetAsync(int id);
+        Task<Movie> PostAsync(Movie movie);
+        Task<Movie> PutAsync(Movie movie);
+        Task<bool> DeleteAsync(int id);
+    }
+}
---
+++ src/MoviesApi.Domain/Interfaces/Services/ViewerService/IViewerService.cs
@@ -0,0 +1,15 @@
+﻿using MoviesApi.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MoviesApi.Domain.Interfaces.Services.ViewerService
+{
+    public interface IViewerService
+    {
+        Task<IEnumerable<Viewer>> GetAllAsync();
+        Task<Movie> GetAsync(int id);
+        Task<Movie> PostAsync(Viewer viewer);
+        Task<Movie> PutAsync(Viewer viewer);
+        Task<bool> DeleteAsync(int id);
+    }
+}
