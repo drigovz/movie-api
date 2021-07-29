28c394d27f69bb739557c77c17d1bd9dda796486
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-07-28 21:57:20 -0300

    feat: setup tests for Infra.CrossCutting

---
+++ tests/MoviesApi.Infra.CrossCutting.Test/AutoMapper.Test/MovieMapperTest.cs
@@ -0,0 +1,8 @@
+﻿using MoviesApi.Infra.CrossCutting.Test.Config;
+
+namespace MoviesApi.Infra.CrossCutting.Test.AutoMapper.Test
+{
+    public class MovieMapperTest: BaseTestService
+    {
+    }
+}
