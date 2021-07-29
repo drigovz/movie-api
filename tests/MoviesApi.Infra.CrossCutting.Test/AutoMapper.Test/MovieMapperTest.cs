f82819aba90b7faaa4fcbddd54d3744d3f3e4bd3
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-07-28 22:31:49 -0300

    test: init tests for dtos

--- tests/MoviesApi.Infra.CrossCutting.Test/AutoMapper.Test/MovieMapperTest.cs
+++ tests/MoviesApi.Infra.CrossCutting.Test/AutoMapper.Test/MovieMapperTest.cs
@@ -1,4 +1,6 @@
 ﻿using MoviesApi.Domain.DTOs.Movies;
+using MoviesApi.Domain.DTOs.MovieViewer;
+using MoviesApi.Domain.DTOs.Viewers;
 using MoviesApi.Domain.Entities;
 using MoviesApi.Infra.CrossCutting.Test.Config;
 using System;
@@ -23,7 +25,7 @@ namespace MoviesApi.Infra.CrossCutting.Test.AutoMapper.Test
                 ReleaseYear = randomYears,
                 DirectedBy = Faker.Name.FullName()
             };
-             
+
             var dtoToEntity = mapper.Map<Movie>(movieDto);
 
             Assert.Equal(movieDto.Id, dtoToEntity.Id);
@@ -31,6 +33,47 @@ namespace MoviesApi.Infra.CrossCutting.Test.AutoMapper.Test
             Assert.Equal(movieDto.Synopsis, dtoToEntity.Synopsis);
             Assert.Equal(movieDto.ReleaseYear, dtoToEntity.ReleaseYear);
             Assert.Equal(movieDto.DirectedBy, dtoToEntity.DirectedBy);
+        }
+
+        [Fact]
+        public void Should_be_possible_to_mapper_ViewerDTO_to_Viewer_Entity()
+        {
+            var randomAge = random.Next(1, 80);
+
+            var viewerDto = new ViewerDTO()
+            {
+                Id = Faker.RandomNumber.Next(100),
+                Name = Faker.Name.FullName(),
+                Email = Faker.Internet.Email(),
+                Age = randomAge,
+                PhoneNumber = Faker.Phone.Number()
+            };
+
+            var dtoToEntity = mapper.Map<Viewer>(viewerDto);
+
+            Assert.Equal(viewerDto.Id, dtoToEntity.Id);
+            Assert.Equal(viewerDto.Name, dtoToEntity.Name);
+            Assert.Equal(viewerDto.Email, dtoToEntity.Email);
+            Assert.Equal(viewerDto.Age, dtoToEntity.Age);
+            Assert.Equal(viewerDto.PhoneNumber, dtoToEntity.PhoneNumber);
+        }
+
+        [Fact]
+        public void Should_be_possible_to_mapper_MovieViewerDTO_to_MovieViewer_Entity()
+        {
+            var randomMovieId = random.Next(1, 60);
+            var randomViewerId = random.Next(70, 100);
+
+            var movieViewerDto = new MovieViewerDTO()
+            {
+                MovieId = randomMovieId,
+                ViewerId = randomViewerId
+            };
+
+            var dtoToEntity = mapper.Map<MovieViewer>(movieViewerDto);
+
+            Assert.Equal(movieViewerDto.MovieId, dtoToEntity.MovieId);
+            Assert.Equal(movieViewerDto.ViewerId, dtoToEntity.ViewerId);
         }
     }
 }
